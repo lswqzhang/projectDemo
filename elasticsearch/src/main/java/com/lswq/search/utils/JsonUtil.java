bcee29ed8446edcabcdf104a6b3db2357ed78e99
zhangshaowei03 <zhangshaowei03@dianping.com>
2017-03-31 10:24:50 +0800

    配置logback文件 增加es查询demo

---
+++ elasticsearch/src/main/java/com/lswq/search/utils/JsonUtil.java
@@ -0,0 +1,36 @@
+package com.lswq.search.utils;
+
+import com.lswq.search.dao.Medicine;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by zhangshaowei on 2017/3/30.
+ */
+public class JsonUtil {
+    /**
+     * 实现将实体对象转换成json对象
+     *
+     * @param medicine Medicine对象
+     * @return
+     */
+    public static String obj2JsonData(Medicine medicine) {
+        String jsonData = null;
+        try {
+            //使用XContentBuilder创建json数据
+            XContentBuilder jsonBuild = XContentFactory.jsonBuilder();
+            jsonBuild.startObject()
+                    .field("id", medicine.getId())
+                    .field("name", medicine.getName())
+                    .field("function", medicine.getFunction())
+                    .endObject();
+            jsonData = jsonBuild.string();
+            System.out.println(jsonData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonData;
+    }
+}
