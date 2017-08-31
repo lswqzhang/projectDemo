2538723e489538c397f4f474280ca99c17d37679
lswqzhang <zhangshaoweilq@163.com>
2017-09-01 00:10:39 +0800

    添加作者信息

--- desginmodel/src/main/java/com/lswq/model/creater/builder/pcbuilder/Client.java
+++ desginmodel/src/main/java/com/lswq/model/creater/builder/pcbuilder/Client.java
@@ -3,6 +3,11 @@ package com.lswq.model.creater.builder.pcbuilder;
 import com.lswq.model.creater.builder.pcbuilder.concreteproduct.MacBook;
 import com.lswq.model.creater.builder.pcbuilder.concreteproduct.SurfacePro;
 
+/**
+ * @author zhangshaoweilq@163.com
+ * <p>
+ * 添加回炉方法的builder的设计模式
+ */
 public class Client {
     public static void main(String[] args) {
         ComputerProduct.Builder builder = new MacBook.Builder();
@@ -18,7 +23,7 @@ public class Client {
 
         ComputerProduct new_apple_display = macBook.newBuilder().buildDisplay("New Apple Display").build();
         ComputerProduct new_lg_display = surfacePro.newBuilder().buildDisplay("New LG Display").build();
-        
+
         System.out.println(new_apple_display.toString());
         System.out.println(new_lg_display.toString());
     }
